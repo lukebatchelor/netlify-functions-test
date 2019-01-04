4a4beae47648a3373678563e3f88b241a1324b6b
Luke Batchelor <lbatchelor@atlassian.com>
2019-01-04 17:42:41 +1100

    Adds secret

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,7 +1,8 @@
 exports.handler = function(event, context, callback) {
   const now = new Date().toString();
+  const secret = process.env.secret || "No secret found";
   callback(null, {
     statusCode: 200,
-    body: `Hello, World - ${now}`
+    body: `Hello, World - ${now}\n${secret}`
   });
 };
